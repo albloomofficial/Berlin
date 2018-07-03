a65588c694451b4cfb081da7ca4e1ba3b16e50a9
Ahmad Yacout <ahmadyacout@Ahmads-MacBook-Pro.local>
2018-07-03 13:00:59 +0400

    first test

--- berlin_staats.py
+++ berlin_staats.py
@@ -4,46 +4,80 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import csv
+from multiprocessing import cpu_count, Pool
+import multiprocessing
 
-with open("/Volumes/LaCie/Stamatov_python/Berlin_results/berlin_publication_numbers.csv", 'a', encoding = 'utf-8') as myfile:
-    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-    wr.writerow(['link', 'year', 'date','number_of_pubs', 'yearly', 'comulativecount'])
+def get_list_years(start_date, end_date):
+    driver = webdriver.Chrome()
+    driver.get('http://zefys.staatsbibliothek-berlin.de/index.php?id=kalender&no_cache=1&tx_zefyskalender_pi1%5Byy%5D=1785')
 
-driver = webdriver.Chrome()
-driver.get('http://zefys.staatsbibliothek-berlin.de/index.php?id=kalender&no_cache=1&tx_zefyskalender_pi1%5Byy%5D=1785')
+    yearlist = driver.find_element_by_xpath('//*[@id="tx-zefyskalender-yy"]').get_attribute('innerHTML')
+    soup = BeautifulSoup(yearlist, 'html.parser')
+    relevantyears = [x.text for x in soup.find_all('option') if (int(x.text) > start_date) and (int(x.text) < end_date)]
+    comulativecount = 0
+    return relevantyears
 
-yearlist = driver.find_element_by_xpath('//*[@id="tx-zefyskalender-yy"]').get_attribute('innerHTML')
-soup = BeautifulSoup(yearlist, 'html.parser')
-relevantyears = [x.text for x in soup.find_all('option') if (int(x.text) > 1779) and (int(x.text) < 1915)]
-comulativecount = 0
+def the_other_part(year):
+    name = f'driver {str(multiprocessing.current_process().name.split("-")[1])}'
+    name = name.replace(' ','')
+    driver = webdriver.Chrome()
+    driver.get('http://zefys.staatsbibliothek-berlin.de/index.php?id=kalender&no_cache=1&tx_zefyskalender_pi1%5Byy%5D=1785')
+    year = str(year)
+    # try:
+    select = Select(driver.find_element_by_xpath('//*[@id="tx-zefyskalender-yy"]'))
+    select.select_by_visible_text(year)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    daylinks = ["http://zefys.staatsbibliothek-berlin.de/{}".format(link.a.attrs['href']) for link in soup.find_all('td', {'class' :'tx-zefyskalender-daymarkiert'})]
+    yearcom = 0
+    comulativecount = 0
+    for thing in daylinks:
+        print('thing')
+        print('{}___{}'.format(name, year))
+        r = requests.get(thing)
+        html = r.text
+        soup = BeautifulSoup(html,'html.parser')
 
-for year in relevantyears:
+        newspapers = soup.find_all('div',{'class' : 'tx-zefyskalender-thumbnail'})
 
-    try:
-        select = Select(driver.find_element_by_xpath('//*[@id="tx-zefyskalender-yy"]'))
-        select.select_by_visible_text(year)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        daylinks = ["http://zefys.staatsbibliothek-berlin.de/{}".format(link.a.attrs['href']) for link in soup.find_all('td', {'class' :'tx-zefyskalender-daymarkiert'})]
-        yearcom = 0
-        for thing in daylinks:
-            r = requests.get(thing)
-            html = r.text
-            soup = BeautifulSoup(html, 'html.parser')
-            newspapers = soup.find_all('div', {'class' : 'tx-zefyskalender-thumbnail'})
-            comulativecount += len(newspapers)
-            yearcom += len(newspapers)
-            print('I have found {} newspapers so far. I am also on year {}'.format(comulativecount, year))
+        comulativecount += len(newspapers)
+        yearcom += len(newspapers)
+        print('Newspaer count: {}. Current year: {}'.format(comulativecount, year))
 
-            with open("/Volumes/LaCie/Stamatov_python/Berlin_results/berlin_publication_numbers.csv", 'a', encoding = 'utf-8') as myfile:
-                wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-                wr.writerow([thing, year, thing.split('=')[-1], str(len(newspapers)), yearcom, comulativecount])
+        with open("berlin_publication_numbers{}.csv".format(name),'a',
+        encoding = 'utf-8') as myfile:
+            wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+            wr.writerow([thing,
+            year,
+            thing.split('=')[-1],
+            str(len(newspapers)),
+            yearcom,
+            comulativecount])
 
-            time.sleep(1)
-    except:
-        driver.close()
-        time.sleep(60*30)
-        driver = webdriver.Chrome()
-        driver.get('http://zefys.staatsbibliothek-berlin.de/index.php?id=kalender&no_cache=1&tx_zefyskalender_pi1%5Byy%5D=1785')
-        select = Select(driver.find_element_by_xpath('//*[@id="tx-zefyskalender-yy"]'))
-        select.select_by_visible_text(year)
-        continue
+        time.sleep(1)
+        print('thing')
+    driver.close()
+    # except:
+    #     driver.close()
+
+
+if __name__ == "__main__":
+    start_date = 1779
+    end_date = 1915
+    year_list = get_list_years(start_date, end_date)
+
+    for i in range(cpu_count()):
+        with open("berlin_publication_numbersdriver{}.csv".format(str(i+1)), 'w') as myfile:
+            wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+            myfile.close()
+
+    with Pool(cpu_count()) as p:
+        p.map(the_other_part, year_list)
+    p.close()
+    p.join()
+
+
+
+    # with open("/Volumes/LaCie/Stamatov_python/Berlin_results/berlin_publication_numbers.csv", 'w', encoding = 'utf-8') as myfile:
+    #     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    #     wr.writerow(['link', 'year', 'date','number_of_pubs', 'yearly', 'comulativecount'])
+    #     myfile.close()
