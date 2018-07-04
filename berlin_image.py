00cd4d68856b4353edc1b0240a11d7e5867ad75a
Ahmad Yacout <ahmadyacout@Ahmads-MacBook-Pro.local>
2018-07-04 08:19:19 +0400

    second image test

--- berlin_image.py
+++ berlin_image.py
@@ -16,8 +16,7 @@ import math
 
 def berlin_scraper(row):
     name = f'driver{str(multiprocessing.current_process().name.split("-")[1])}'
-    print('{}: I am actually working hehe'.format(name))
-
+    pattern = "http://zefys.staatsbibliothek-berlin.de"
     result = requests.get(row)
     homepage = result.content
     soup = BeautifulSoup(homepage, "html.parser")
@@ -28,7 +27,7 @@ def berlin_scraper(row):
         newspapernumber += 1
         time.sleep(2)
         page = 1
-        r = requests.get('http://zefys.staatsbibliothek-berlin.de/{}'.format(thing))
+        r = requests.get('{}/{}'.format(pattern, thing))
         html = r.text
         soup = BeautifulSoup(html, 'html.parser')
         test = soup.find('a', {'class' : 'imglink'})
@@ -39,8 +38,6 @@ def berlin_scraper(row):
         location = meta_data[2].strip().replace(' ', '_')
         date = meta_data[3].strip().replace(' ', '_')
 
-        print('I got this far!')
-
         with open("index_{}.csv".format(name), 'a', encoding = 'utf-8') as myfile:
             wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
             wr.writerow([title, date, location])
@@ -49,37 +46,42 @@ def berlin_scraper(row):
         directory = "Berlin_results/{}/{}/{}".format(location,title,date)
 
         try:
-            print('{}: working on {} in {} from  {}'.format(name, title, location, date))
+            print('{}: working on {} in {} from  {}'.format(name,
+            title, location, date))
             os.makedirs(directory)
 
         except OSError as e:
             if e.errno == errno.EEXIST:
                 print('{}: same article'.format(name))
 
-
-
-            # with eventlet.Timeout(20):
-        urllib.request.urlretrieve(image, "Berlin_results/{}/{}/{}/{}{}.jpg".format(location,title,date,title,page))
-
+        urllib.request.urlretrieve(image,
+        "Berlin_results/{}/{}/{}/{}{}.jpg".format(location,
+        title,
+        date,
+        title,page))
 
         nextpage = soup.find('a', {'class': 'next'})
 
         while nextpage != None:
             try:
                 page += 1
-                r = requests.get('http://zefys.staatsbibliothek-berlin.de/{}'.format(nextpage['href']))
+                r = requests.get('{}/{}'.format(pattern, nextpage['href']))
                 html = r.text
                 soup = BeautifulSoup(html, 'html.parser')
                 test = soup.find('a', {'class' : 'imglink'})
                 image = test.img['src']
                 # with eventlet.Timeout(20):
-                urllib.request.urlretrieve(image, "Berlin_results/{}/{}/{}/{}{}.jpg".format(location,title,date,title,page))
+
+                urllib.request.urlretrieve(image,
+                "Berlin_results/{}/{}/{}/{}{}.jpg".format(location,
+                title,
+                date,
+                title,page)
+                )
+
                 nextpage = soup.find('a', {'class': 'next'})
             except:
                 pass
-
-
-
 
 
 if __name__ == "__main__":
@@ -91,21 +93,9 @@ if __name__ == "__main__":
         reader = csv.reader(f)
         list_of_pubs = list(reader)
 
-    # pages = sum(1 for row in list_of_pubs)
-    # increment = math.ceil(pages / (multiprocessing.cpu_count()*3/4))
-    # print(increment)
     list_of_rows = [row[0] for row in list_of_pubs]
     print('created the list of rows')
     with Pool(cpu_count()) as p:
         p.map(berlin_scraper, list_of_rows , chunksize = 10)
     p.close()
     p.join()
-
-    # pool = multiprocessing.Pool()
-    # for i in range(int(multiprocessing.cpu_count()*3/4)):
-    #     print('starting this whole thing up')
-    #     url_range = increment * i
-    #     new_process = multiprocessing.Process(name=driver_names[i], target=berlin_scraper, args = (url_range, increment, i))
-    #     new_process.start()
-    # pool.close()
-    # pool.join()
